bf4f634a78b61194c6142cee0ecd6650693a52c5
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-11-15 01:02:00 +0530

    Prisma Backend - User Routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,30 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
 
-app.post('/api/v1/user/signup', (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  const response = await prisma.user.create({
+    data:{
+      name: body.name,
+      email: body.email,
+      password: body.password
+    }
+  })
+
   return c.text('signup');
 })
 
