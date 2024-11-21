2041bb7992ef337b7dab1865ea560ecfd9051879
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-11-21 20:23:09 +0530

    updated blogs route : get

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -133,6 +133,16 @@ blogRouter.get('/:id', async (c) => {
     const blog = await prisma.post.findFirst({
         where: {
             id: id
+        },
+        select: {
+            id: true,
+            title: true,
+            content: true,
+            author: {
+                select: {
+                    name: true
+                }
+            }
         }
     })
 
